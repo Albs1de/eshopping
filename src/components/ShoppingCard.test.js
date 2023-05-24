a05a7c7b7e53311887c3c9dcf8992cc2db45467b
Albs1de <dkoskovi@stud.hs-offenburg.de>
2023-05-24 11:42:58 +0200

    Write first test for ShoppingCard.js

---
+++ src/components/ShoppingCard.test.js
@@ -0,0 +1,37 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+import ShoppingCard from "./ShoppingCard";
+
+describe("ShoppingCard", () => {
+  // Überprüft ob der Titel und die Beschreibung korrekt gerendert werden
+  test("renders the title and description", () => {
+    render(
+      <ShoppingCard
+        title="Test Title"
+        description="Test Description"
+        onClick={() => {}}
+      />
+    );
+
+    expect(screen.getByText("Test Title")).toBeInTheDocument();
+    expect(screen.getByText("Test Description")).toBeInTheDocument();
+  });
+
+  // Test der überprüft, ob die onClick-Funktion aufgerufen wird wenn der Button geklickt wird.
+  test("calls onClick when the button is clicked", () => {
+    const handleClick = jest.fn();
+
+    render(
+      <ShoppingCard
+        title="Test Title"
+        description="Test Description"
+        onClick={handleClick}
+      />
+    );
+
+    userEvent.click(screen.getByText("Buy now"));
+
+    expect(handleClick).toHaveBeenCalled();
+  });
+});
