9c43961831406aff6887addf8c1ef4aae2ef211f
Albs1de <dkoskovi@stud.hs-offenburg.de>
2023-05-24 14:11:22 +0200

    Fix: delete setShoppingItems

--- src/components/ShoppingCardList.js
+++ src/components/ShoppingCardList.js
@@ -4,65 +4,64 @@ import { CartContext } from "./ShoppingCardProvider";
 
 const ShoppingCardList = () => {
   const { addToCart } = useContext(CartContext);
-  const [shoppingItems, /* eslint-disable-next-line */ setShoppingItems] =
-    useState([
-      {
-        id: 1,
-        title: "Shoes",
-        description: "Nice shoes...",
-        price: 25,
-        count: 0,
-      },
-      {
-        id: 2,
-        title: "Sunglasses",
-        description: "Cool sunglasses...",
-        price: 50,
-        count: 0,
-      },
-      {
-        id: 3,
-        title: "Shirt",
-        description: "Nice shirt...",
-        price: 75,
-        count: 0,
-      },
-      {
-        id: 4,
-        title: "Hat",
-        description: "Nice hat...",
-        price: 100,
-        count: 0,
-      },
-      {
-        id: 5,
-        title: "Socks",
-        description: "Nice socks...",
-        price: 125,
-        count: 0,
-      },
-      {
-        id: 6,
-        title: "Pants",
-        description: "Nice pants...",
-        price: 150,
-        count: 0,
-      },
-      {
-        id: 7,
-        title: "Jacket",
-        description: "Nice jacket...",
-        price: 25,
-        count: 0,
-      },
-      {
-        id: 8,
-        title: "Gloves",
-        description: "Nice gloves...",
-        price: 175,
-        count: 0,
-      },
-    ]);
+  const [shoppingItems] = useState([
+    {
+      id: 1,
+      title: "Shoes",
+      description: "Nice shoes...",
+      price: 25,
+      count: 0,
+    },
+    {
+      id: 2,
+      title: "Sunglasses",
+      description: "Cool sunglasses...",
+      price: 50,
+      count: 0,
+    },
+    {
+      id: 3,
+      title: "Shirt",
+      description: "Nice shirt...",
+      price: 75,
+      count: 0,
+    },
+    {
+      id: 4,
+      title: "Hat",
+      description: "Nice hat...",
+      price: 100,
+      count: 0,
+    },
+    {
+      id: 5,
+      title: "Socks",
+      description: "Nice socks...",
+      price: 125,
+      count: 0,
+    },
+    {
+      id: 6,
+      title: "Pants",
+      description: "Nice pants...",
+      price: 150,
+      count: 0,
+    },
+    {
+      id: 7,
+      title: "Jacket",
+      description: "Nice jacket...",
+      price: 25,
+      count: 0,
+    },
+    {
+      id: 8,
+      title: "Gloves",
+      description: "Nice gloves...",
+      price: 175,
+      count: 0,
+    },
+  ]);
 
   return (
     <div className="flex flex-wrap justify-center">
