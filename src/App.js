b746807fe5e14fa873a2f3286a44698c60cb9198
Albs1de <dkoskovik@gmail.com>
2023-06-18 14:11:07 +0200

    Delete Login from App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import ShoppingCardList from "./components/ShoppingCardList";
 import { CartProvider } from "./components/ShoppingCardProvider";
-import UserLogin from "./components/Login";
 function App() {
   return (
     <>
-      <UserLogin />
       <CartProvider>
         <Navbar />
         <ShoppingCardList />
