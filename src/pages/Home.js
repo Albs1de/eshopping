afcd4a9e4a7acd35ef527cf68db6ede30bf2f33b
Albs1de <dkoskovi@stud.hs-offenburg.de>
2023-07-09 13:31:47 +0200

    Update footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ const Homepage = () => {
   const [showShoppingList, setShowShoppingList] = useState(false);
 
   return (
-    <div className="bg-gray-200 p-10 flex flex-col items-center">
+    <div className="bg-gray-200 p-2 flex flex-col items-center">
       <div className="bg-gray-200 p-10 flex flex-col items-center">
         {!showShoppingList && (
           <>
@@ -34,22 +34,17 @@ const Homepage = () => {
           Rabatt und erhalten Sie ein Bonusgeschenk mit jeder Bestellung
         </p>
         <div className="flex items-center justify-center">
-          <input
-            className="rounded px-4 py-2 w-full"
-            type="email"
-            placeholder="Deine Email..."
-          ></input>
           <button
             className="ml-2 px-2 py-2 bg-black text-white rounded w-20"
             type="submit"
           >
-            Eintragen
+            Jetzt beitreten
           </button>
         </div>
       </div>
       <div className="bg-gray-200 p-10 w-full flex flex-col items-center text-center mx-20">
         <h2 className="text-lg my-2 font-bold">Bestsellers</h2>
-        <p className="text-base my-2 ">
+        <p className="text-base my-2 text-slate-500 ">
           Erleben Sie die Raffinesse unserer sorgfältig kuratierten Kollektion
           hochwertiger Kleidungsstücke. Verwöhnen Sie sich in Stil und Eleganz
           auf unserer E-Commerce-Plattform, die für jeden Anlass das passende
@@ -61,6 +56,39 @@ const Homepage = () => {
         >
           Shoppen
         </button>
+      </div>
+      <div className="bg-white w-full flex flex-col items-center">
+        <p className="text-lg">Sei immer up to date und trage dich ein</p>
+        <div className="flex flex-row my-4 items-center">
+          <input
+            className="rounded px-4 py-2 "
+            type="email"
+            placeholder="Deine Email..."
+          ></input>
+          <button
+            className="ml-2 px-2 py-2 bg-black text-white rounded w-20"
+            type="submit"
+          >
+            Eintragen
+          </button>
+        </div>
+
+        <div className="flex flex-row  ">
+          <div className="ml-2 py-5">
+            {" "}
+            <h1 className="font-bold mt-10 text-xl">eShopping</h1>
+            <h1 className="font-bold">Hilfe</h1>
+            <p className="text-slate-500">FAQ</p>
+            <p className="text-slate-500">Customer Service</p>
+            <p className="text-slate-500">Kontaktiere uns</p>
+          </div>
+          <div className="ml-10 px-2 py-5 mt-16">
+            <h1 className="font-bold">Andere</h1>
+            <p className="text-slate-500">Privacy Policy</p>
+            <p className="text-slate-500">Sitemap</p>
+            <p className="text-slate-500">Mitgliedschaft</p>
+          </div>
+        </div>
       </div>
     </div>
   );
